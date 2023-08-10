d77a2e197309a9b03cb687348908dc1f1a4272a9
Patrick Kwan <101650875+ptkwan@users.noreply.github.com>
2023-08-10 10:20:54 -0300

    Add files via upload

---
+++ conversao.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var a int = 20
+var b string = "Nome"
+
+func main() {
+
+	var c float64 = float64(a) // tipo (variavel)
+	fmt.Printf("O valor de C é: %g   e o valor de B é:%s   ", c, b)
+
+}
